04341a8bbda685e2724b33b83144ccd15c9ad0d0
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-10-31 10:57:09 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
-
+import {heroVideo, smallHeroVideo }from '../utils'
 
 const Hero = () => {
+
+  const [videoSrc,setVideoSrc]=useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo )
+
+  const handleVideoSrcSet = () =>{
+    if(window.innerWidth<760){
+      setVideoSrc(smallHeroVideo)
+    }
+    else{
+      setVideoSrc(heroVideo)
+    }
+  } 
+
+  useEffect( () => {
+    window.addEventListener('resize',handleVideoSrcSet);
+
+    return () =>{
+      window.removeEventListener('resize',handleVideoSrcSet);
+    }
+  } ,[])
 
   useGSAP( ()=>{
     gsap.to('#hero',{
@@ -17,8 +36,8 @@ const Hero = () => {
       <div className='h-5/6 w-full flex-center flex-col'>
       <p id='hero' className='hero-title'>iPhone 15 Pro</p>
       <div className='md:w-10/12 w-9/12'>
-        <video>
-          <source src={videoSrc} />
+        <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
+          <source src={videoSrc} type='video/mp4' />
         </video>
       </div>
       </div>
