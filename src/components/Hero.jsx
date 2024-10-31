c23b16ffb6e1f06d6f7b5717fc659f18fc7b82d2
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-10-31 10:41:31 +0530

    2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
 
 const Hero = () => {
+
+  useGSAP( ()=>{
+    gsap.to('#hero',{
+      opacity:1,
+      delay:1.5,
+      duration:1,
+    })
+  },[])
   return (
-    <div>Hero</div>
+    <section className="w-full nav-height bg-black relative">
+      <div className='h-5/6 w-full flex-center flex-col'>
+      <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+      <div className='md:w-10/12 w-9/12'>
+        <video>
+          <source src={videoSrc} />
+        </video>
+      </div>
+      </div>
+
+    </section>
   )
 }
 
