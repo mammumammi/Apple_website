e695b5f542353a81320a2001c938c46dacd7d2a8
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-11-16 19:48:29 +0530

    l

---
+++ src/components/Model.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Model = () => {
+  return (
+    <section className='common-padding'>
+        <div className='screen-max-width'>
+            <h1 id="heading" className='section-heading
+            '></h1>
+        </div>
+    </section>
+  )
+}
