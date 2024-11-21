0928e069b7582f65ab59325de3ce2bd3b7fb9257
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-11-21 13:07:35 +0530

    a

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,22 +1,35 @@
 import { PerspectiveCamera, View } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React from 'react';
+import React, { Suspense } from 'react';
+import Lights from './Lights';
+import { div } from 'three/webgpu';
+import IPhone from './iPhone';
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
+    
+      
     <Canvas className="w-full h-full">
       <View
         index={index}
         id={gsapType}
         className={`border-2 border-red-500 w-full h-full ${
           index === 2} ? 'right-[-100%]' : ''
-        `}
-      >
+          `}
+          >
         <ambientLight intensity={0.3} />
 
         <PerspectiveCamera makeDefault position={[0,0,4]} />
+        <Lights />
+
+        <Suspense
+        fallback={<div>Loading</div>}
+        >
+        <IPhone />
+        </Suspense>
       </View>
     </Canvas>
+          
   );
 };
 
