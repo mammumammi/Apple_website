0c6a798d223810b2637ce260ea19212d12d411d8
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-11-21 12:45:27 +0530

    2

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,23 +1,23 @@
-import { View } from '@react-three/drei';
+import { PerspectiveCamera, View } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React from 'react'
+import React from 'react';
 
-
-const ModelView = ({ index, groupRef,gsapType,controlRef,setRotationSize,size, item}) => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
-    <Canvas>
-    <View
-     index={index}
-     id={gsapType}
-     className={`border-2 border-red-500 w-full h-full
-       ${index ===2} ? 'right-[-100%] : '' `}
-     >
-      <ambientLight intensity={0.3} />
+    <Canvas className="w-full h-full">
+      <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full ${
+          index === 2} ? 'right-[-100%]' : ''
+        `}
+      >
+        <ambientLight intensity={0.3} />
 
-     </View>
-
+        <PerspectiveCamera makeDefault position={[0,0,4]} />
+      </View>
     </Canvas>
-  )
-}
+  );
+};
 
 export default ModelView;
