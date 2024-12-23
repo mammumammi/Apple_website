611302f20b937446c1b89cbbeaa687da62531327
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-12-23 12:02:19 +0530

    k

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,4 +1,4 @@
-import { section } from 'framer-motion/client'
+
 import React, { useRef } from 'react'
 import { chipImg, frameImg, frameVideo } from '../utils'
 import { useGSAP } from '@gsap/react'
